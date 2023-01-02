b18f634956a440d7d09df88407d5d576b02d0661
dukelester <dukelester4@gmail.com>
2023-01-03 00:56:22 +0300

    New creates an empty object and starts adding the methods and object properties.
    The new uses a constructor method which creates an object/instance.

--- OOP/intro.js
+++ OOP/intro.js
@@ -15,14 +15,31 @@ const triangle = {
     a: 7,
     b: 10,
     hypotenuse: function () {
-        return Math.sqrt(this.a**2 + this.b**2);
+        return Math.sqrt(this.a ** 2 + this.b ** 2);
     },
     area: function() {
         return (this.a * this.b ) / 2;
     },
     perimeter: function() {
-        return this.hypotenuse() + this.a + this.b
+        return this.hypotenuse() + this.a + this.b;
     }
 }
 
-console.log(triangle.a, triangle.b, triangle.area(), triangle.hypotenuse(), triangle.perimeter())
+console.log(triangle.a, triangle.b, triangle.area(), triangle.hypotenuse(), triangle.perimeter());
+
+// The contructor
+function TriangleConstructor(height, base) {
+    this.height = height;
+    this.base = base;
+    this.getArea = function() {
+        return (this.height * this.base) / 2;
+    };
+    this.hypotenuse = function() {
+        return Math.sqrt(this.height ** 2 + this.base ** 2);
+    };
+}
+
+const myTriangle = new TriangleConstructor(78, 20);
+console.log(myTriangle.height, myTriangle.base, myTriangle.getArea(), myTriangle.hypotenuse());
+const myTriangle2 = new TriangleConstructor(79, 78);
+console.log(myTriangle2.height, myTriangle2.base, myTriangle2.getArea(), myTriangle2.hypotenuse());
