a39fba7d263cff2693e920af933f9c248dafd796
dukelester <dukelester4@gmail.com>
2023-01-02 23:15:51 +0300

    Added the set, clear, delete, keys methods to the map

--- datastructures.js
+++ datastructures.js
@@ -12,3 +12,16 @@ const functionCalls = new Map();
 functionCalls.set(add, 0);
 functionCalls.set(mult, 0);
 console.log(functionCalls);
+
+const marks = [
+    ['john', 90], ['mark', 89], ['ken', 12], ['mary', 90],
+];
+const marksMap = new Map(marks);
+console.log(marksMap);
+marksMap.set('Elven', 78).set('Moses', 78).set('Peter', 89);
+console.log(marksMap);
+console.log(marksMap.has('ken'));
+const keys = marksMap.keys();
+console.log('keys', keys, [...keys]);
+marksMap.clear();
+console.log(marksMap);
