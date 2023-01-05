8fe80b9c40aa3a3a08612bc0daad18c0304ce960
dukelester <dukelester4@gmail.com>
2023-01-05 15:08:32 +0300

    Introduction to datastructures and algorithms in JavaScript..
    Looking at different worst cases for example scenarios.

---
+++ DataStructures/README.md
@@ -0,0 +1,11 @@
+# JavaScript Datastructures and Algorithms
+
+The Datastructures and algorithms are the methods of storage and set of rules to access data
+and carry out data operations.
+
+## The O(n) notation
+
+O(1) => Order 1 the constant time for space and time.
+O(n) => Linear
+O(n^2) => Quadratic
+O(n^3) => cubic
---
+++ DataStructures/intro.js
@@ -0,0 +1,42 @@
+const fullName = 'lester dlester'; // The order 1 => O(1)
+console.log(fullName);
+
+// Linear => O(n)
+const linearFunction = (n) => {
+  for (let k = 0; k < n; k += 1) {
+    console.log(k);
+  }
+};
+linearFunction(8);
+
+// Quadratic => O(n^2)
+const quadraticFunction = (n) => {
+  for (let i = 0; i < n; i += 1) {
+    console.log('I in first loop => ', i);
+    for (let m = i; m < n; m += 1) {
+      console.log('m in second loop => ', m);
+      console.log(m * i);
+    }
+  }
+};
+
+quadraticFunction(8);
+
+// Cubic => O(n^3)
+const cubicFunction = (n) => {
+  for (let i = 0; i < n; i += 1) {
+    console.log(`The I from loop 1 => ${i}`);
+    for (let m = i; m < n; m += 1) {
+      console.log(`The m from loop 2 => ${m}`);
+      console.log(m * i);
+      for (let k = m; k < n; k += 1) {
+        console.log(`The K from loop 3 => ${k}`);
+        console.log(k * m * i);
+      }
+    }
+  }
+};
+
+cubicFunction(8);
+
+// Logarithimic algorithm
