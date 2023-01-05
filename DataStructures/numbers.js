fa1006ca2c3a45d12e08ed78a6a2764ec0429f3d
dukelester <dukelester4@gmail.com>
2023-01-05 17:16:41 +0300

    Time Complexity: O(sqrt(n)) This improved solution cuts the time complexity down significantly.

--- DataStructures/numbers.js
+++ DataStructures/numbers.js
@@ -13,3 +13,18 @@ const isPrime = (number) => {
 
 console.log(isPrime(-2), isPrime(72));
 console.log(isPrime(2), isPrime(7));
+
+const isPrimeImproved = (number) => {
+  if (number <= 1) return false;
+  if (number <= 3) return true;
+  if (number % 2 === 0 || number % 3 === 0) return false;
+  for (let k = 5; k * k < number; k += 6) {
+    if (number % k === 0 || number % (k + 2) === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(isPrimeImproved(2), isPrimeImproved(20), isPrimeImproved(6), isPrimeImproved(9));
+console.log(isPrimeImproved(99), isPrimeImproved(57));
