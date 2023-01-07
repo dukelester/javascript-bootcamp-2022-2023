45430d92ca9ba52590273b5f11c14e7e52391273
dukelester <dukelester4@gmail.com>
2023-01-07 16:01:44 +0300

    Given a matrix representing a tic-tac-toe board, determine whether someone won, whether it was a tie, or whether the game has not ended yet

--- DataStructures/arrayAdvanced.js
+++ DataStructures/arrayAdvanced.js
@@ -39,3 +39,78 @@ const spiralPrint = (matrix) => {
   }
 };
 spiralPrint(myMatrix);
+
+// TIC-TAC-TOE CHECK ==>  Given a matrix representing a tic-tac-toe board, determine whether
+// someone won, whether it
+// was a tie, or whether the game has not ended yet.
+
+const checkRow = (rowArray, letter) => {
+  for (let k = 0; k < 3; k += 1) {
+    if (rowArray[k] !== letter) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const checkColumn = (gameBoardMatrix, columnIndex, letter) => {
+  for (let i = 0; i < 3; i += 1) {
+    if (gameBoardMatrix[i][columnIndex] !== letter) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const ticTacToeWinner = (gameBoardMatrix, letter) => {
+  // Check rows
+  const rowWin = checkRow(gameBoardMatrix[0], letter)
+  || checkRow(gameBoardMatrix[1], letter)
+  || checkRow(gameBoardMatrix[2], letter);
+
+  const columnWin = checkColumn(gameBoardMatrix, 0, letter)
+  || checkColumn(gameBoardMatrix, 1, letter)
+  || checkColumn(gameBoardMatrix, 2, letter);
+
+  const diagonalWinLeftToRight = gameBoardMatrix[0][0] === letter
+  && gameBoardMatrix[1][1] === letter
+  && gameBoardMatrix[2][2] === letter;
+
+  const diagonalWindRightToLeft = gameBoardMatrix[0][2] === letter
+  && gameBoardMatrix[1][1] === letter
+  && gameBoardMatrix[2][1] === letter;
+
+  return rowWin || columnWin || diagonalWinLeftToRight || diagonalWindRightToLeft;
+};
+
+const board = [
+  ['O', '-', 'X'],
+  ['-', 'O', '-'],
+  ['-', 'X', 'O'],
+];
+const board2 = [
+  ['X', '-', 'X'],
+  ['-', 'X', '-'],
+  ['X', 'X', 'X'],
+];
+
+const board3 = [
+  ['-', '-', '-'],
+  ['-', 'X', '-'],
+  ['O', '-', 'X'],
+];
+console.log(board);
+console.log('*****game 1*********', board);
+console.log(ticTacToeWinner(board, 'X')); // X -wins
+console.log(ticTacToeWinner(board, 'O')); // O wins
+console.log(ticTacToeWinner(board3, '-')); // draw
+console.log('*****game 2*********', board2);
+
+console.log(ticTacToeWinner(board2, 'X'));
+console.log(ticTacToeWinner(board2, 'O'));
+console.log(ticTacToeWinner(board3, '-'));
+console.log('*****game 3*********', board3);
+
+console.log(ticTacToeWinner(board3, 'X'));
+console.log(ticTacToeWinner(board3, 'O'));
+console.log(ticTacToeWinner(board3, '-'));
