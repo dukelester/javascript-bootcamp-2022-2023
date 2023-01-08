2c237f04fa27143fbbbb9a50853faf9a99ebc1e9
dukelester <dukelester4@gmail.com>
2023-01-08 11:13:51 +0300

    Added the Js objects

---
+++ DataStructures/objects.js
@@ -0,0 +1,11 @@
+// JavaScript Objects
+const user = {};
+user.username = 'lester456789',
+user.phoneNumber = '98745682224';
+user.location = 'USA';
+user.marks = [45, 67, 89, 90, 56, 34];
+user.fullName = function fullName() {
+  return 'duke lester';
+};
+
+console.log(user);
