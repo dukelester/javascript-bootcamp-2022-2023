b0f17e2eaa0c80c81a31994d950b60be5d276a1f
dukelester <dukelester4@gmail.com>
2023-01-08 11:35:10 +0300

    The fibonacci of a number => number - 1 + number - 2

--- DataStructures/recursion.js
+++ DataStructures/recursion.js
@@ -15,6 +15,10 @@ In recursion, there must be a base case (also referred to as terminating case). 
 recursive methods call themselves, they will never stop unless this base case is reached.
 Stack overflow from recursion is most likely the result of not having a proper base case.
 In the base case, there are no recursive function calls.
+  * Divide-and-Conquer Method
+   * the divide-and-conquer method is when a problem is solved by
+   * solving all of its smaller components
+   *
  */
 
 const countDownToZero = (number) => {
@@ -24,4 +28,15 @@ const countDownToZero = (number) => {
   console.log(number);
   countDownToZero(number - 1);
 };
-countDownToZero(100);
+// countDownToZero(100);
+
+// The fibonacci of a number => number - 1 + number - 2
+const fibonacci = (number) => {
+  if (number <= 1) { // Base case
+    return number;
+  }
+  return fibonacci(number - 1) + fibonacci(number - 2); // time complexity of O(2n)
+};
+console.log(fibonacci(4));
+console.log(fibonacci(10));
+console.log(fibonacci(16));
