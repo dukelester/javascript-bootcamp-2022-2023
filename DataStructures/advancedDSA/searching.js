701bbd63fe4399021f3d6333056fd9e1e947a07f
dukelester <dukelester4@gmail.com>
2023-01-09 15:24:50 +0300

    Binary search
    The binary search algorithm is fast but can be done only if the array is sorted. It
    checks the middle element if that is the element that is being searched for. If the search
    element is bigger than the middle element, the lower bound is set to the middle element
    plus one. If the search element is less than the middle element, the higher bound is set to
    the middle element minus one.

--- DataStructures/advancedDSA/searching.js
+++ DataStructures/advancedDSA/searching.js
@@ -25,3 +25,26 @@ console.log(linearSearch(nums, 56));
 const linearSearchTwo = (numArray, element) => numArray.findIndex((num) => num === element);
 console.log(linearSearchTwo(nums, 34));
 console.log(linearSearchTwo(nums, 56));
+
+// The Binary Search ==> Applicable for a sorted array
+const binarySearch = (sortedNumsArray, element) => {
+  let lastIndex = sortedNumsArray.length - 1;
+  let startIndex = 0;
+  while (startIndex <= lastIndex) {
+    const mid = Math.floor((lastIndex + startIndex) / 2);
+    if (sortedNumsArray[mid] === element) {
+      return `Found at ${mid}`;
+    }
+    if (element > sortedNumsArray[mid]) {
+      startIndex = mid;
+    } else {
+      lastIndex = mid;
+    }
+  }
+  return -1;
+};
+
+const sortedNums = [12, 14, 26, 56, 78, 89, 90, 98, 99, 100, 200, 230];
+console.log(binarySearch(sortedNums, 99));
+console.log(binarySearch(sortedNums, 99));
+console.log(binarySearch(sortedNums, 12));
