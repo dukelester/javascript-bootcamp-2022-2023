772d79fcc70c20b4d558d1d0a48744ba64930ac4
dukelester <dukelester4@gmail.com>
2023-01-09 21:30:26 +0300

    Selection sort
    Time Complexity: O(n2)
    Space Complexity: O(1)
    The time complexity for selection sort is still O(n2) because of the nested for loop

--- DataStructures/advancedDSA/sorting.js
+++ DataStructures/advancedDSA/sorting.js
@@ -29,3 +29,29 @@ const nums = [23, 78, 34, 67, 20, 12, 23, 90, 55];
 const marks = [10, 67, 34, 89, 98, 78, 45, 70, 34, 67, 34, 45, 25];
 console.log(bubbleSort(nums));
 console.log(marks);
+
+// Selection Sort
+/** Selection sorting works by scanning the elements for the smallest element and inserting
+ * it into the current position of the array. This algorithm is marginally better than bubble sort
+ */
+const selectionSort = (items) => {
+  const length = items.length;
+  let min = 0;
+  for (let i = 0; i < length; i += 1) {
+    min = i;
+    // check the rest of the array to see if anything is smaller
+    for (let j = i + 1; j < length; j += 1) {
+      if (items[j] < items[min]) {
+        min = j;
+      }
+    }
+    // if the minimum isn't in the position, swap it
+    if (i !== min) {
+      swap(items, i, min);
+    }
+  }
+  return items;
+};
+
+console.log(selectionSort(marks));
+console.log(selectionSort(nums));
